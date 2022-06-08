009a64df4d2247c33d01f3d052d745e64489eac0
spavythra <spavythra@gmail.com>
2022-06-08 20:13:50 +0300

    quotation removed

--- Myto-do/public/store/index.js
+++ Myto-do/public/store/index.js
@@ -1,7 +1,7 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import {todosReducer} from './TodoStore/TodoReducer'
 import {FilterTodoReducer} from './FilterTodoStore/FilterTodoReducer'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage' 
 
