32942709dca2c04eedccea3fa61a7bad8316f3ba
spavythra <spavythra@gmail.com>
2022-06-08 19:55:40 +0300

    quotation removed

--- Myto-do/public/store/index.js
+++ Myto-do/public/store/index.js
@@ -3,7 +3,7 @@ import {todosReducer} from './TodoStore/TodoReducer'
 import {FilterTodoReducer} from './FilterTodoStore/FilterTodoReducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' ;
+import storage from 'redux-persist/lib/storage' 
 
 const persistConfig = {
     key: 'root',
